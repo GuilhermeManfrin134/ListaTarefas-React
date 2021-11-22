57d4dba0f3c9d2d0865d58097a6914e03737ebd7
GuiManfrin134 <manfrin11gui@gmail.com>
2021-11-22 17:35:43 -0300

    Sistema de checagem melhorado

--- src/componentes/Task.jsx
+++ src/componentes/Task.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import "./Task.css"
 import { CgCloseO, CgInfo } from 'react-icons/cg'
+import { AiFillCheckCircle } from 'react-icons/ai'
 
 const Task = ({ task, handleTaskClick, handleTaskRemove }) => {
     //return <div className="task-container">{task.title}</div>
     return (
         <div 
-            className="task-container" 
-            style={task.completed ? {borderLeft: '15px solid crimson'} : {}}
+            className="task-container"
+            style={task.completed ? {backgroundColor: 'crimson'} : {}} 
         >
-
+            <div className="completed-task" style={task.completed ? {display: 'flex', backgroundColor: 'crimson', fontSize: '25px', margin: '0', padding: '0'} : {}}>
+                <AiFillCheckCircle />
+            </div>
             <div className="task-title"
                 onClick={() => handleTaskClick(task.id)}
             >
@@ -18,12 +21,12 @@ const Task = ({ task, handleTaskClick, handleTaskRemove }) => {
 
             <div className="buttons-container">
                 <button 
-                    className="remove-task-button"
+                    className="remove-task-button"  style={task.completed ? {backgroundColor: 'crimson'} : {}} 
                 >
                     <CgInfo />
                 </button>
                 <button 
-                    className="remove-task-button"
+                    className="remove-task-button"  style={task.completed ? {backgroundColor: 'crimson'} : {}} 
                     onClick={() => handleTaskRemove(task.id)}
                 >
                     <CgCloseO />
